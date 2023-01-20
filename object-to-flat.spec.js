c1f44fdec9acd17f1c8d2c8a08d98f8aa76f9c60
Maksim Kalenich <maxim.kalenich@gmail.com>
2023-01-20 22:29:05 +0200

    test: fail, added new test case with simple single key:value pair
    it fails second case due to lack of clean state from previous run

--- object-to-flat.spec.js
+++ object-to-flat.spec.js
@@ -3,39 +3,53 @@ import { describe, it } from 'mocha';
 import { assert } from 'chai';
 
 describe('Split object into array of key:value pairs', () => {
-  it('complex object without self reference', () => {
+  it('single key:value object test', () => {
     const asserted = {
-      a: {
-        b: {
-          c: 1,
-          d: 2,
-          g: {
-            k: null,
-            l: ['foo', 'bar']
-          },
-          o: 'foo'
-        },
-        m: 'bar',
-        e: [123, 123],
-      },
-      n: 'bar',
-      f: 'foo',
+      a: 'b',
     };
 
     const expected = [
-      { 'a.b.c': 1 },
-      { 'a.b.d': 2 },
-      { 'a.b.g.k': null },
-      { 'a.b.g.l': ['foo', 'bar'] },
-      { 'a.b.o': 'foo' },
-      { 'a.m': 'bar' },
-      { 'a.e': [123, 123] },
-      { n: 'bar' },
-      { f: 'foo' },
+      { 'a': 'b' },
     ];
 
     for (let [i, keyVal] of getFlatObject(asserted).entries()) {
       assert.deepStrictEqual(keyVal, expected[i]);
     }
   });
+
+  // it('complex object without self reference', () => {
+  //   const asserted = {
+  //     a: {
+  //       b: {
+  //         c: 1,
+  //         d: 2,
+  //         g: {
+  //           k: null,
+  //           l: ['foo', 'bar']
+  //         },
+  //         o: 'foo'
+  //       },
+  //       m: 'bar',
+  //       e: [123, 123],
+  //     },
+  //     n: 'bar',
+  //     f: 'foo',
+  //   };
+
+  //   const expected = [
+  //     { 'a.b.c': 1 },
+  //     { 'a.b.d': 2 },
+  //     { 'a.b.g.k': null },
+  //     { 'a.b.g.l': ['foo', 'bar'] },
+  //     { 'a.b.o': 'foo' },
+  //     { 'a.m': 'bar' },
+  //     { 'a.e': [123, 123] },
+  //     { n: 'bar' },
+  //     { f: 'foo' },
+  //   ];
+
+  //   for (let [i, keyVal] of getFlatObject(asserted).entries()) {
+  //     assert.deepStrictEqual(keyVal, expected[i]);
+  //   }
+  // });
 });
